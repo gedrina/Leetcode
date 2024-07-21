b65ede27165cc973832828e1c5c688acdc33f388
gedrina <edrinagashi@gmail.com>
2024-07-21 23:28:46 +0100

    169

---
+++ 169_majority_element.py
@@ -0,0 +1,22 @@
+def majorityElement(nums):
+        counts = {}
+        
+        #create hash map
+        for num in nums:
+            if num in counts:
+                counts[num] += 1
+            else:
+                counts[num] = 1
+        
+        #find mos frequent value
+        for num, count in counts.items():
+            if count > len(nums)//2:
+                return num
+
+# Example 1
+nums1 = [3, 2, 3]
+print(majorityElement(nums1))  # Output: 3
+
+# Example 2
+nums2 = [2, 2, 1, 1, 1, 2, 2]
+print(majorityElement(nums2))  # Output: 2
