185a3036374b2443d2b28dffde041ff1fd88fe75
gedrina <edrinagashi@gmail.com>
2024-07-25 23:19:24 +0100

    1160

---
+++ 1160_find_words_that_can_be_formed_by_characters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countCharacters(self, words, chars: str) -> int:
+        from collections import Counter
+        from collections import defaultdict
+      
+
+    # If we find that a word is not good we set is_good to 0
+    #This ensures that only the lengths of good words are added to the result
+
+        count = Counter(chars)
+        result = 0
+
+        for w in words:
+            current = defaultdict(int)
+            is_good = 1
+            for char in w:
+                current[char] += 1
+                if char not in count or current[char] > count[char]:
+                    is_good = 0
+                    break
+            result += len(w) * is_good
+        
+        return result
+    
