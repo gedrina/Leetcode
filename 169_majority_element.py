89f83d912ff0c517c4417997dcac6de050bacd86
gedrina <edrinagashi@gmail.com>
2024-07-29 21:18:51 +0100

    add second solution

--- 169_majority_element.py
+++ 169_majority_element.py
@@ -20,3 +20,12 @@ print(majorityElement(nums1))  # Output: 3
 # Example 2
 nums2 = [2, 2, 1, 1, 1, 2, 2]
 print(majorityElement(nums2))  # Output: 2
+
+# second solution
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         nums_count = Counter(nums)
+
+#         for key in nums_count:
+#             if nums_count[key] > len(nums) // 2:
+#                 return key
