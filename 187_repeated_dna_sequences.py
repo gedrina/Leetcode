4df73553b8179e257071c6aee33b930f33b3418d
gedrina <edrinagashi@gmail.com>
2024-08-06 15:40:14 +0100

    add 187

---
+++ 187_repeated_dna_sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) :
+        adn = set()
+        r_adn = set()
+
+        for i in range(len(s)-10+1):
+            if s[i:i+10] in adn:
+                r_adn.add(s[i:i+10])
+            else:
+                adn.add(s[i:i+10])
+        return r_adn
+
