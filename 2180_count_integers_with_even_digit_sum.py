dd48fc81ea3068f085b34f2ae3a04f147e4c80f0
gedrina <edrinagashi@gmail.com>
2024-08-08 12:26:24 +0100

    add some advancments

--- 2180_count_integers_with_even_digit_sum.py
+++ 2180_count_integers_with_even_digit_sum.py
@@ -1,21 +1,21 @@
-num = 12
-count = 0
-
-for number in range(1,num+1):
-    check = str(number)
-    suma = 0
-    
-    for digit in check:
-        suma += int(digit)
-    if suma % 2 == 0:
-        count += 1
-print(count)
-    
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+        for number in range(1,num+1):
+            current = str(number)
+            suma = 0
+            for digit in current:
+                suma += int(digit)
+            if suma % 2 == 0:
+                count += 1
+        return count 
 
 
-        
-    
 
 
         
-    
+
+       
+
+         
+        
