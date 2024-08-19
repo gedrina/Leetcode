b5bcd2ad5963402ff5148284d377a60d3befcdcc
gedrina <edrinagashi@gmail.com>
2024-08-19 20:04:03 +0100

    second solution

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -15,7 +15,29 @@ class Solution:
         
         return answer
 
+# second solution
+# from typing import List
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         right = [1]
+#         left = [1]
+#         nums_rev = nums[::-1]
+
+#         for i in range(len(nums)-1):
+#             value_r = nums[i] * right[-1]
+#             right.append(value_r)
+#         for j in range(len(nums)-1): 
+#             value_l = nums_rev[j] * left[-1]
+#             left.append(value_l)
+
+#         left_rev = left[::-1]
+#         print(left_rev)
+#         print(right)
+#         answer = []
+#         for x in range(len(nums)):
+#             answer.append(right[x]*left_rev[x])
+#         return answer
 
 # Exmaple
 nums = [1,2,3,4]
